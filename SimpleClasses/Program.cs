d0287e559ba19c66178a8f6e753f2899e6f8fbfa
Justin Knauf <justin@gooten.com>
2016-11-22 17:57:36 -0500

    adding code

--- SimpleClasses/Program.cs
+++ SimpleClasses/Program.cs
@@ -10,8 +10,40 @@ namespace SimpleClasses
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("");
+            Car myCar = new Car();
+            myCar.Make = "Acura";
+            myCar.Model = "Integra Type R";
+            myCar.Year = 2000;
+            myCar.Color = "Phoenix Yellow";
+
+            Console.WriteLine("{0} {1} {2} {3}",
+                myCar.Make,
+                myCar.Model,
+                myCar.Year,
+                myCar.Color);
+
+            decimal value = DetermineMarketValue(myCar);
+            Console.WriteLine("{0:C}", value);
+
+
             Console.ReadLine();
         }
+
+        private static decimal DetermineMarketValue(Car car)
+        {
+            decimal carValue = 100.0M;
+
+
+            return carValue;
+        }
+    }
+
+    class Car
+    {
+        public string Make { get; set; }
+        public string Model { get; set; }
+        public int Year { get; set; }
+        public string Color { get; set; }
+
     }
 }
